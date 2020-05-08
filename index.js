83fd8733f5825cc523f1db54c4b0108b02e97efe
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-05-09 00:24:04 +0530

    mobile nav fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const clickMobileRes = document.querySelector("#change-img");
 const headerNav = document.getElementById("header-nav");
 const headerLinks = document.getElementById("nav-links");
+const mobileHide = document.querySelector(".mobile-hide");
+var mediaNavDisplay = window.matchMedia("(max-width: 480px)");
 let isToggle = true;
 clickMobileRes.addEventListener("click", function () {
   if (isToggle) {
@@ -15,11 +17,24 @@ clickMobileRes.addEventListener("click", function () {
   }
 });
 function changeNavStyle() {
-  headerNav.style.display = "block";
+  //header links
+  headerNav.style.display = "flex";
+  headerNav.style.justifyContent = "center";
   headerNav.style.position = "absolute";
   headerNav.style.top = "5rem";
   headerNav.style.width = "100%";
+  //nav Links
+  headerLinks.style.flexDirection = "column";
+  headerLinks.style.alignItems = "center";
+  headerLinks.style.background = "rgb(255,255,255)";
+  headerLinks.style.color = "hsl(233, 26%, 24%)";
+  headerLinks.style.width = "80%";
+  headerLinks.style.padding = "2rem 0";
+  headerLinks.style.borderRadius = "8px";
+  headerLinks.style.minHeight = "15rem";
 }
 function changeToDefaultNav() {
-  headerNav.style.display = "none";
+  if (mediaNavDisplay.matches) {
+    headerNav.style.display = "none";
+  }
 }
